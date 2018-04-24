e88fe419a624cb4427d5ee4e79f964cd13f329aa
vignesht96 <vignesht.96@gmail.com>
2018-04-24 12:42:28 -0700

    Updated with Routes and redirect to small homepage

--- BlastOffEnterprise/settings.py
+++ BlastOffEnterprise/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'BlastOffEnterprise.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'Pages/Home')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
